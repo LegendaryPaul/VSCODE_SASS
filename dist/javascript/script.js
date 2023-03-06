67a3ab2ca3b8ceb6015311969ccfb56ee517845d
LegandaryPaul <paulanjelozzz@gmail.com>
2023-03-06 15:49:12 +0800

    updated slider

--- dist/javascript/script.js
+++ dist/javascript/script.js
@@ -1,6 +1,12 @@
 const toggle = document.querySelector(".toggle__menu");
 const nav = document.querySelector(".header__nav");
 
+
+
 toggle.addEventListener("click", () => {
     nav.classList.toggle("open");
+    toggle.classList.toggle("open");
 })
+
+
+
